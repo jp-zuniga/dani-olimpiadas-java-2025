3037abb37639abc3a937d742e71e79566b27bd2c
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-11-19 15:06:37 -0600

    documentar

--- fase-1/countVowels.java
+++ fase-1/countVowels.java
@@ -2,18 +2,28 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
+    // Definir vocales
     public static List<String> vowels = Arrays.asList("a", "e", "i", "o", "u");
 
     public static void main(String[] args) {
+        // Imprimir resultado de llamado de función
         System.out.println(countVowels("Olimpiadas de Java"));
     }
 
     public static int countVowels(String text) {
+        // Inicializar conteo
         int vowelCount = 0;
 
+        // Iterar sobre caracteres de string
         for (int i = 0; i < text.length(); i++) {
+            // Extraer caracter con .substring():
+            // // Le pasamos el índice actual como inicio, y el próximo índice
+            // // como fin para solo conseguir un solo caracter.
+            // // Ese caracter se lo pasamos .contains() para verificar si esta
+            // // en la lista de vocales.
             if (vowels.contains(text.substring(i, i + 1))) {
-                vowelCount++;
+                // Incrementar conteo
+                vowelCount += 1;
             }
         }
 
